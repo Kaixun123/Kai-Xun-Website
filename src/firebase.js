e94e05db3b7d7873d65368c6eae81965ef09bbbf
Juay Kai Xun <juaykaixun@hotmail.com>
2023-12-21 11:42:12 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const analytics = getAnalytics(app);
